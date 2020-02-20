a5eec7bf9d22f621cd1d331bc5108280276a92c7
Jay Kappel <jay@comsite.org>
2020-02-20 15:23:01 -0700

    jk - add support for getting content type from the curlinfo

--- src/VCR/Util/CurlHelper.php
+++ src/VCR/Util/CurlHelper.php
@@ -105,6 +105,9 @@ class CurlHelper
             case CURLINFO_HTTP_CODE:
                 $info = (int)$response->getStatusCode();
                 break;
+            case CURLINFO_CONTENT_TYPE:
+                $info = $response->getHeader('Content-Type');
+                break;
             case CURLINFO_SIZE_DOWNLOAD:
                 $info = $response->getHeader('Content-Length');
                 break;
@@ -187,7 +190,7 @@ class CurlHelper
                 break;
         }
     }
-    
+
     /**
      * Makes sure we've properly handled the POST body, such as ensuring that
      * CURLOPT_INFILESIZE is set if CURLOPT_READFUNCTION is set.
@@ -201,13 +204,13 @@ class CurlHelper
         if (is_null($readFunction)) {
             return;
         }
-        
+
         // Guzzle 4 sometimes sets the post body in CURLOPT_POSTFIELDS even if
         // they have already set CURLOPT_READFUNCTION.
         if ($request->getBody()) {
             return;
         }
-        
+
         $bodySize = $request->getCurlOption(CURLOPT_INFILESIZE);
         Assertion::notEmpty($bodySize, 'To set a CURLOPT_READFUNCTION, CURLOPT_INFILESIZE must be set.');
         $body = call_user_func_array($readFunction, array($curlHandle, fopen('php://memory', 'r'), $bodySize));
