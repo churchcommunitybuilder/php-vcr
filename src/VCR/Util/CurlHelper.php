3c8ca9e3d953417bebcd5bc72b99f18fb9e7b012
Jay Kappel <jay@comsite.org>
2020-02-21 10:59:33 -0700

    jk - added case for content type as the values are case sensitive

--- src/VCR/Util/CurlHelper.php
+++ src/VCR/Util/CurlHelper.php
@@ -105,6 +105,9 @@ class CurlHelper
             case CURLINFO_HTTP_CODE:
                 $info = (int)$response->getStatusCode();
                 break;
+            case CURLINFO_CONTENT_TYPE:
+                $info = $response->getHeader('Content-Type');
+                break;
             case CURLINFO_SIZE_DOWNLOAD:
                 $info = $response->getHeader('Content-Length');
                 break;
